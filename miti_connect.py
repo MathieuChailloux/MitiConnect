bc2b4af80267d07c041088d78cb7e9f7a632f1d7
Mathieu Chailloux <fdrmc@free.fr>
2023-11-06 19:25:15 +0100

    metadata update + icon attempt

--- miti_connect.py
+++ miti_connect.py
@@ -271,7 +271,7 @@ class MitiConnect:
     def initGui(self):
         """Create the menu entries and toolbar icons inside the QGIS GUI."""
 
-        icon_path = ':/plugins/MitiConnect/icon.png'
+        icon_path = ':/plugins/MitiConnect/icon2.png'
         self.add_action(
             icon_path,
             text=self.pluginName,

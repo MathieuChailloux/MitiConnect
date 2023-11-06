d8ffdffdb8c3a902d0428334e6e38477f7bd96b3
Mathieu Chailloux <fdrmc@free.fr>
2023-11-06 20:47:07 +0100

    Add MitiConnect icon

--- miti_connect.py
+++ miti_connect.py
@@ -271,7 +271,7 @@ class MitiConnect:
     def initGui(self):
         """Create the menu entries and toolbar icons inside the QGIS GUI."""
 
-        icon_path = ':/plugins/MitiConnect/icon2.png'
+        icon_path = ':/plugins/MitiConnect/icons/icon.png'
         self.add_action(
             icon_path,
             text=self.pluginName,

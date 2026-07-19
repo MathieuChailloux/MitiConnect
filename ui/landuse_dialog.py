65e1b586cf887d651f75415f5f1fc5ef845924bb
Mathieu Chailloux <fdrmc@free.fr>
2026-07-19 19:08:00 +0200

    Fix  landuse merge opening

--- ui/landuse_dialog.py
+++ ui/landuse_dialog.py
@@ -54,7 +54,8 @@ class LanduseDialogModel(abstract_model.DictModel):
     def __init__(self, name, string_list,pluginModel):
         #itemClass = utils.getModuleRelativePath(__name__,LanduseDialogItem.__name__)
         try:
-            itemClass = getattr(sys.modules[__name__], LanduseDialogItem.__name__)
+            # itemClass = getattr(sys.modules[__name__], LanduseDialogItem.__name__)
+            itemClass = LanduseDialogItem
             super().__init__(itemClass=itemClass,feedback = pluginModel.feedback)
             self.pluginModel = pluginModel
             # self.feedback = pluginModel.feedback
